ef556c5e4dabf21ed1d740afe533545b289ea57c
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-27 20:09:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ puts aod
 #while
 
 
-#def directors_totals(nds)
+def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
